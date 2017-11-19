12014647f98cf47ff85c19c5cd87c25bee62e904
Erika Miguel <ms.erikamiguel@gmail.com>
2017-11-19 16:33:33 -0500

    enforcing ttl

--- api/login/tests/tests.py
+++ api/login/tests/tests.py
@@ -47,7 +47,8 @@ class LoginModulesTests(unittest.TestCase):
         response = authorizer.authorize(credentialsDictionary["username"], credentialsDictionary["password"])
         self.assertTrue(response["token"])
         self.assertTrue(response["ttl"])
-        self.assertTrue(response["created"])
+        self.assertTrue(response["createdTime"])
+        self.assertTrue(response["expireTime"])
 
         # A user that does not exist
         credentials = SimpleCredentials("fakeuser", "fakepassword")
