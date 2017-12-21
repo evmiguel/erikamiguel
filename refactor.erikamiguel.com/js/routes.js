3f53114cf89da503457590b8d397692e78aa0894
Erika Miguel <ms.erikamiguel@gmail.com>
2017-12-20 21:31:26 -0500

    #3 combined navigo with ynon prek's tutorial to make component htmls

--- refactor.erikamiguel.com/js/routes.js
+++ refactor.erikamiguel.com/js/routes.js
@@ -8,19 +8,31 @@ var useHash = false; // Defaults to: false
 var hash = '#!'; // Defaults to: '#'
 var router = new Navigo(root, useHash, hash);
 
-function loadHTML(url, customElement) {
-  $.get(url).then(res => document.querySelector('main').innerHTML = res )
-}
+// Creating Page Layouts
+var welcome = new Page("welcome.html")
+var notFound = new Page("notFound.html")
+var home = new Layout(new Page("menu.html"), new Page("home.html"))
+var blog = new Layout(new Page("menu.html"), new Page("blog.html"))
+var about = new Page("blog.html");
 
+
+async function loadHTML(layout, customElement) {
+	await layout.load()
+	var el = document.querySelector(customElement)
+	el.innerHTML = ''
+	layout.show(el)
+ }
+
+// Define routes i.e. erikamiguel.com/home, erikamiguel.com/blog, etc.
 router.on({
-  'home': () => { loadHTML('views/menu.html') },
-  'about': () => { loadHTML('views/about.html') }
+  'home': () => { loadHTML(home, 'main') },
+  'blog': () => { loadHTML(blog, 'main') },
+  'about': () => { loadHTML(home, 'main'); loadHTML(about, 'homeData'); }
 });
 
 // Set main page
-router.on(() => loadHTML('views/welcome.html'));
+router.on(() => loadHTML(welcome, 'main'));
 
 // Also paraphrasing Navigo Demo here...
-router.notFound((query) => { loadHTML('views/notFound.html') });
-
+router.notFound((query) => { loadHTML(notFound, 'main') });
 router.resolve();
