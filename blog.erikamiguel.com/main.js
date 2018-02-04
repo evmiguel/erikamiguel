8fa7af53e0a7a3eabe4cf0fbc50ba571928fbf81
Erika Miguel <ms.erikamiguel@gmail.com>
2018-02-04 09:35:30 -0500

    #6 getting metadata from dynamodb and htmlcontent from s3

--- blog.erikamiguel.com/main.js
+++ blog.erikamiguel.com/main.js
@@ -37,16 +37,13 @@ var GenerateUI = function()	{
 		});
 
 		/**************************************************************
-		* GET HTML CONTENT FROM S3 FILE
+		* INITIALIZE AWS RESOURCES
 		***************************************************************/
+		this.dynamodb = new AWS.DynamoDB(AWS.config.credentials);
 		this.s3 = new AWS.S3(AWS.config.credentials)
-		var params = {
-				Bucket: bucket, 
-		};
-		this.s3.listObjects(params, function(err, data) {
-			if (err) console.log(err, err.stack); // an error occurred
-			else { this.processObjects(data.Contents) }
-		}.bind(this));
+
+		this.getBlogPosts();
+		
 	},
 
 	/***********************************************************************
@@ -61,23 +58,74 @@ var GenerateUI = function()	{
 		el.innerHTML = currentHTML + html;
 	},
 
-	this.processObjects = function(s3Objects){
-		var params = {
-			Bucket: bucket
-		};
+	/***********************************************************************
+	* GET HTML CONTENT FROM S3
+	************************************************************************/
+	this.getHTMLContent = function(s3Objects){
 		for (var s3Obj of s3Objects){
-			params.Key = s3Obj.Key
+			var params = {
+				Bucket: s3Obj.s3_bucket.S,
+				Key: s3Obj.s3_bucket_prefix.S + "/" + s3Obj.filename.S
+			};
+
+			// OBJECT TO BIND TO CALLBACK
+			var obj = { "obj" : this,
+						"title" : s3Obj.title.S,
+						"author" : s3Obj.author.S,
+						"date" : epochToDate(s3Obj.date.N)
+					};
+
 			this.s3.getObject(params, function(err, data){
 				if (err) console.log(err, err.stack);
-				else { 
-					content = new TextDecoder("utf-8").decode(data.Body);
-					this.generateUI(content); 
+				else {
+					var title = "<h1>"+this.title+"</h1>";
+					var author = "<h2>Author: "+this.author+"</h2>";
+					var date = "<h3>Date: "+this.date+"</h3>";
+					var metaContent = title + author + date;
+					var bodyContent = new TextDecoder("utf-8").decode(data.Body);
+					var content = metaContent + bodyContent;
+					this.obj.generateUI(content); 
 				}
-			}.bind(this));
-		}
+			}.bind(obj));
+		};
+	},
+
+	/***********************************************************************
+	* GET BLOG POSTS FROM DYNAMODB
+	************************************************************************/
+	this.getBlogPosts = function(){
+		var params = { TableName: blogTable};
+		this.dynamodb.scan(params, function(err, data){
+			if (err) console.log(err, err.stack); // an error occurred
+   			else {
+   				postData = data.Items.sort(function(p1,p2){
+   					var a = parseInt(p1.id.N);
+   					var b = parseInt(p2.id.N);
+   					if (a < b){ return -1; }
+   					if (a > b){ return 1; }
+   					return 0;
+   				});
+   				
+   				filteredPostData = postData.filter(function(obj){
+   					return "title" in obj; // check if object has title key
+   				});
+   				this.getHTMLContent(filteredPostData);
+   			}
+		}.bind(this));
 	}
 
 	this.init()
+}
+
+/**************************************************************
+*
+* UTILITY FUNCTIONS
+*
+**************************************************************/
+function epochToDate(datetimeString){
+	var d = new Date(0);
+	d.setUTCSeconds(datetimeString);
+	return d;
 }
 
 
